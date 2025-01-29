c72d38ebc522025ec97320d9ea28534fab10382e
uunite <avivhagay004@gmail.com>
2025-01-29 09:06:56 +0200

    Added contentStyle.css and styled feature button

--- content.js
+++ content.js
@@ -6,41 +6,37 @@ window.addEventListener('mouseup', () => {
 	const selection = window.getSelection()
 	selectedText = selection.toString()
 	//Remove last highlight container
-	if (document.getElementById('highlight-container')) {
-		document.getElementById('highlight-container').remove()
+	if (document.getElementById('BT-highlight-container')) {
+		document.getElementById('BT-highlight-container').remove()
 	}
 
 	if (selectedText == lastSelectedText || !selectedText) {
-		// no selected text
-		//or
-		//bug fix when you click the same selected text
+		// no selected text OR bug fix when you click the same selected text
 		return
 	}
 
 	// all clear
 
 	const rect = selection.getRangeAt(0).getBoundingClientRect()
-	createContainer(rect.top, rect.left, rect.width, rect.height)
+	const cont = createContainer(rect.top, rect.left, rect.width, rect.height)
+	createFeatButton('Copy', () => console.log('Hello'), 'ICON', cont)
 })
 
 const createContainer = (top, left, width, height) => {
 	const cont = document.createElement('div')
-	cont.style.all = 'unset'
-	cont.style.backgroundColor = 'red'
-	cont.style.position = 'absolute'
+	cont.classList.add('BT-highlight-container')
+	cont.setAttribute('id', 'BT-highlight-container')
 	cont.style.top = `${top + window.scrollY}px`
 	cont.style.left = `${left + window.scrollX}px`
 	cont.style.width = `${width}px`
 	cont.style.height = `${height}px`
-	cont.style.zIndex = '99999'
 	document.body.appendChild(cont)
-	cont.setAttribute('id', 'highlight-container')
 	return cont
 }
 
 const createFeatButton = (featName, featFunc, featIcon, container) => {
 	const featButton = document.createElement('button')
-	featButton.classList.add('feat-button')
+	featButton.classList.add('BT-feat-btn')
 	featButton.value = featName
 	//Need to put featIcon inside the button
 	//Add a little popup with the featName
